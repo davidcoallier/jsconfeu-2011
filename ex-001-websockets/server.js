7c83e327b1d5798868595d2a803484da66a0efbb
David Coallier <david@echolibre.com>
2011-10-02 12:31:21 +0200

    Added a few comments to make it easier to showcase.

--- ex-001-websockets/server.js
+++ ex-001-websockets/server.js
@@ -5,22 +5,24 @@ var io = require('socket.io').listen(app);
 var fs = require('fs');
 app.listen(80);
 
+// Disable the layout. 
 app.configure(function(){
     app.set("view options", {layout: false});
 });
 
-
+// Set the static file directory for express.
 app.use(express.static(__dirname + '/'));
 
-
-
+// First route which is the main "/" route.
 app.get('/', function(req, res) {
     fs.readFile(__dirname + '/index.html', 'utf8', function(err, text){
         res.send(text);
     });
 });
+
 var nicknames = {};
 
+// The basic default chat app.
 io.sockets.on('connection', function (socket) {
    socket.on('user message', function (msg) {
        socket.broadcast.emit('user message', socket.nickname, msg);
